3d7c3f916a158f38af1aa7c46d7e8c57b2e5afa8
Jerry Chen <18901787009@163.com>
2024-11-09 14:36:10 +0800

    MBTI测试，纯前端，需要npm install 安装依赖后即可运行

---
+++ mbti-test-mini/dist/common.js
@@ -0,0 +1,52 @@
+"use strict";
+(wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./src/components/GlobalFooter/index.tsx":
+/*!***********************************************!*\
+  !*** ./src/components/GlobalFooter/index.tsx ***!
+  \***********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+/* harmony import */ var _tarojs_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @tarojs/components */ "./node_modules/@tarojs/plugin-platform-weapp/dist/components-react.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "webpack/container/remote/react/jsx-runtime");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+
+
+
+/**
+ * 全局底部栏组件
+ * @author Jerry
+ *
+ */
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_tarojs_components__WEBPACK_IMPORTED_MODULE_1__.View, {
+    className: "globalFooter",
+    children: "\u4F5C\u8005\uFF1AJerry"
+  });
+});
+
+/***/ }),
+
+/***/ "./src/assets/headerBg.jpg":
+/*!*********************************!*\
+  !*** ./src/assets/headerBg.jpg ***!
+  \*********************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "assets/headerBg.jpg";
+
+/***/ }),
+
+/***/ "./src/data/questions.json":
+/*!*********************************!*\
+  !*** ./src/data/questions.json ***!
+  \*********************************/
+/***/ (function(module) {
+
+module.exports = /*#__PURE__*/JSON.parse('[{"options":[{"result":"I","value":"独自工作","key":"A"},{"result":"E","value":"与他人合作","key":"B"}],"title":"你通常更喜欢"},{"options":[{"result":"J","value":"喜欢有明确的计划","key":"A"},{"result":"P","value":"更愿意随机应变","key":"B"}],"title":"当安排活动时"},{"options":[{"result":"T","value":"认为应该严格遵守","key":"A"},{"result":"F","value":"认为应灵活运用","key":"B"}],"title":"你如何看待规则"},{"options":[{"result":"E","value":"经常是说话的人","key":"A"},{"result":"I","value":"更倾向于倾听","key":"B"}],"title":"在社交场合中"},{"options":[{"result":"J","value":"先研究再行动","key":"A"},{"result":"P","value":"边做边学习","key":"B"}],"title":"面对新的挑战"},{"options":[{"result":"S","value":"注重细节和事实","key":"A"},{"result":"N","value":"注重概念和想象","key":"B"}],"title":"在日常生活中"},{"options":[{"result":"T","value":"更多基于逻辑分析","key":"A"},{"result":"F","value":"更多基于个人情感","key":"B"}],"title":"做决定时"},{"options":[{"result":"S","value":"喜欢有结构和常规","key":"A"},{"result":"N","value":"喜欢自由和灵活性","key":"B"}],"title":"对于日常安排"},{"options":[{"result":"P","value":"首先考虑可能性","key":"A"},{"result":"J","value":"首先考虑后果","key":"B"}],"title":"当遇到问题时"},{"options":[{"result":"T","value":"时间是一种宝贵的资源","key":"A"},{"result":"F","value":"时间是相对灵活的概念","key":"B"}],"title":"你如何看待时间"},{"options":[{"result":"E","value":"参加大型社交活动","key":"A"},{"result":"I","value":"参加小型聚会或一对一交流","key":"B"}],"title":"在社交活动中"},{"options":[{"result":"S","value":"关注具体细节","key":"A"},{"result":"N","value":"关注整体大局","key":"B"}],"title":"处理信息时"},{"options":[{"result":"T","value":"坚持客观标准","key":"A"},{"result":"F","value":"考虑他人感受","key":"B"}],"title":"做决策时"},{"options":[{"result":"J","value":"事先规划","key":"A"},{"result":"P","value":"即兴发挥","key":"B"}],"title":"面对日常任务时"},{"options":[{"result":"I","value":"独处充电","key":"A"},{"result":"E","value":"社交充电","key":"B"}],"title":"恢复精力时"},{"options":[{"result":"P","value":"探索多种可能性","key":"A"},{"result":"J","value":"专注于单一目标","key":"B"}],"title":"设定目标时"},{"options":[{"result":"N","value":"创新和发明","key":"A"},{"result":"S","value":"实用和应用","key":"B"}],"title":"对待新想法时"},{"options":[{"result":"F","value":"和谐与共识","key":"A"},{"result":"T","value":"真理和公正","key":"B"}],"title":"价值观上"},{"options":[{"result":"J","value":"有组织的环境","key":"A"},{"result":"P","value":"自由自在的环境","key":"B"}],"title":"偏好工作环境时"},{"options":[{"result":"E","value":"表达自己的观点","key":"A"},{"result":"I","value":"倾听他人的意见","key":"B"}],"title":"在团队讨论中"},{"options":[{"result":"S","value":"收集数据","key":"A"},{"result":"N","value":"直觉感知","key":"B"}],"title":"获取信息时"},{"options":[{"result":"T","value":"逻辑和条理","key":"A"},{"result":"F","value":"同情和温暖","key":"B"}],"title":"处理人际关系时"},{"options":[{"result":"J","value":"完成任务","key":"A"},{"result":"P","value":"探索新想法","key":"B"}],"title":"面对工作项目时"},{"options":[{"result":"I","value":"深入思考","key":"A"},{"result":"E","value":"广泛交流","key":"B"}],"title":"在个人成长中"},{"options":[{"result":"N","value":"想象未来","key":"A"},{"result":"S","value":"回忆过去","key":"B"}],"title":"思考时"},{"options":[{"result":"F","value":"适应他人","key":"A"},{"result":"T","value":"坚持原则","key":"B"}],"title":"在社交互动中"},{"options":[{"result":"P","value":"开放和随意","key":"A"},{"result":"J","value":"严谨和有序","key":"B"}],"title":"处理生活事务时"}]');
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common.js.map
